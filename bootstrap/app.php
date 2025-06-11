92b635c85b0e8d06ccfb236e4fc66ef10e03881d
Rogério Guimarães <dossantosfilho.rogerio525@gmail.com>
2025-06-11 08:41:02 -0300

    removido stateful

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-        $middleware->append(EnsureFrontendRequestsAreStateful::class);
-    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
