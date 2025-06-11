11108373fdad907571f388e74a272feaaced339f
Rogério Guimarães <dossantosfilho.rogerio525@gmail.com>
2025-06-11 08:47:27 -0300

    handle cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -11,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+        $middleware->append(HandleCors::class);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
